d98c8eb2658c6d386e352d3f0b4f4d0a15888471
Jesus Ponce <chuyp11@gmail.com>
2018-06-04 13:20:38 -0500

    04/06/18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Angular Animations
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { AltaMateriasComponent } from './components/administrar-materias/alta-materias/alta-materias.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AltaMateriasComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
